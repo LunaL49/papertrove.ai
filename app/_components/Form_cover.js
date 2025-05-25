ba1fc01ce46f33b8244bcdeedc3cc74e4d10e880
LunaQL <luna.q.y.li@gmail.com>
2025-05-25 18:52:35 +0100

    added journals page

--- app/_components/Form_cover.js
+++ app/_components/Form_cover.js
@@ -3,63 +3,54 @@ import { motion } from "framer-motion";
 export default function Form_cover({ handleNext }) {
   return (
     <>
-      <div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0 }}
-          className="text-2xl font-bold mb-8 text-gray-700"
-        >
-          Hi! Welcome to
-          <span className="bg-gradient-to-r from-[#0fa3b1] to-[#f7a072] bg-clip-text text-transparent font-bold">
-            {" "}
-            Papertrove.ai{" "}
-          </span>
-        </motion.div>
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0 }}
+        className="text-2xl font-bold mb-6 text-gray-700"
+      >
+        Hi! Welcome to
+        <span className="bg-gradient-to-r from-[#0fa3b1] to-[#f7a072] bg-clip-text text-transparent font-bold">
+          {" "}
+          Papertrove.ai{" "}
+        </span>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1 }}
+        className="text-[18px] mb-4 text-gray-700"
+      >
+        Let
+        <span className="bg-gradient-to-r from-[#0fa3b1] to-[#f7a072] bg-clip-text text-transparent font-bold">
+          {" "}
+          Papertrove{" "}
+        </span>
+        be your personal companion when it comes to finding new and exciting
+        research.
+      </motion.div>
 
-      <div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1 }}
-          className="text-[18px] mb-4 text-gray-700"
-        >
-          Let
-          <span className="bg-gradient-to-r from-[#0fa3b1] to-[#f7a072] bg-clip-text text-transparent font-bold">
-            {" "}
-            Papertrove{" "}
-          </span>
-          be your personal companion when it comes to finding new and exciting
-          research.
-        </motion.div>
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2 }}
+        className="text-[18px] mb-4 text-gray-700 justify-center"
+      >
+        Set your preferences, and wait for the latest research digest to land in
+        your inbox! Handpicked articles fresh off the press, all tailored to
+        your interests by a little ML algorithm running behind the scenes.
+      </motion.div>
 
-      <div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 2 }}
-          className="text-[18px] mb-4 text-gray-700 justify-center"
-        >
-          Set your preferences, and wait for the latest research digest to land
-          in your inbox! Handpicked articles fresh off the press, all tailored
-          to your interests by a little ML algorithm running behind the scenes.
-        </motion.div>
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 3 }}
+        className="text-[18px] mb-6 text-gray-700 justify-center"
+      >
+        And it is free! All you need to do is fill out one tiny form, so...
+      </motion.div>
 
-      <div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 3 }}
-          className="text-[18px] mb-8 text-gray-700 justify-center"
-        >
-          And it is free! All you need to do is fill out one tiny form, so...
-        </motion.div>
-      </div>
-
-      <div className="w-full flex justify-left">
+      <div className="w-full flex justify-left mb-2">
         <motion.button
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -70,6 +61,34 @@ export default function Form_cover({ handleNext }) {
           Let us start! (Works best on a computer.)
         </motion.button>
       </div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 4 }}
+        className="text-sm mb-4 text-gray-500"
+      >
+        See{" "}
+        <a
+          href="/journals"
+          style={{
+            textDecoration: "none",
+            color: "#0fa3b1",
+            transition: "all 0.3s",
+          }}
+          onMouseEnter={(e) => {
+            e.target.style.color = "#f7a072";
+            e.target.style.textDecoration = "underline";
+          }}
+          onMouseLeave={(e) => {
+            e.target.style.color = "#0fa3b1";
+            e.target.style.textDecoration = "none";
+          }}
+        >
+          papertrove.ai/journals
+        </a>{" "}
+        for a list of currently support journals.
+      </motion.div>
     </>
   );
 }
---
+++ app/journals/page.js
@@ -0,0 +1,234 @@
+export default function Journals() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full">
+      <div className="flex flex-col w-full h-4/5 rounded-[4rem] overflow-hidden border my-5 bg-gray-50 border-gray-200 shadow-2xl items-center justify-center">
+        <div className="w-3/4 h-3/4 ">
+          <div>
+            <h2 className="text-lg font-bold text-gray-700 mb-6">
+              Currently,{" "}
+              <span className="bg-gradient-to-r from-[#0fa3b1] to-[#f7a072] bg-clip-text text-transparent font-bold">
+                {" "}
+                Papertrove.ai{" "}
+              </span>{" "}
+              selects research articles from these journals:
+            </h2>
+          </div>
+
+          <div className="flex flex-col h-[320px] overflow-auto">
+            <p className="text-sm mb-1 font-normal text-gray-500">Science</p>
+            <p className="text-sm mb-1 font-normal text-gray-500">
+              Science Signalling
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Science Translational Medicine
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Science Advances
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Science Immunology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Science Robotics
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Nature</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Aging
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Astronomy
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Biomedical Engineering
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Biotechnology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Cancer
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Cardiovascular Research
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Catalysis
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Cell Biology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Chemical Biology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Chemical Engineering
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Chemistry
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Cities
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Climate Change
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Communications
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Computational Science
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Ecology and Evolution
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Electronics
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Energy
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Food
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Genetics
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Geoscience
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Human Behaviour
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Immunology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Machine Intelligence
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Materials
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Medicine
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Mental Health
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Metabolism
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Methods
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Microbiology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Nanotechnology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Neuroscience
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Photonics
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Physics
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Plants
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Protocols
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Structural and Molecular Biology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Sustainability
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nature Synthesis
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Scientific Reports
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Cell</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cancer Cell
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Metabolism
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Genomics
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Chemical Biology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Host and Microbe
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Reports
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Reports Medicine
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Stem Cell
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Systems
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Current Biology
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Developmental Cell
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Immunity</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Med</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Molecular Cell
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Neuron</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Structure</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Reports Physical Science
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Chem</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Chem Catalysis
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Device</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Joule</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Matter</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Reports Methods
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Cell Reports Sustainability
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Heliyon</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">iScience</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">One Earth</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Patterns</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              STAR Protocols
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Proceedings of the National Academy of Science
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Bioinformatics
+            </p>
+            <p className="text-sm mb-2 font-normal text-gray-500">Brain</p>
+            <p className="text-sm mb-2 font-normal text-gray-500">
+              Nucleic Acids Research
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
